4b9a62d440b54a5b4929174a7cd3854f92187ad7
Julia Rybska <rybska.julia@gmail.com>
2026-01-18 00:32:54 +0100

    FlashAttention working but not for max batch size - needs a fiz

--- lab4/report.md
+++ lab4/report.md
@@ -3,3 +3,11 @@
 - I don't have a GPU locally, so I had to setup things on Athena, it took a while
 - The setup works now - training complete on CUDA-enabled GPU
 - the lab-related experiments are work in progress!!
+
+- added random seed so runs are reproducible and (more importantly) comparable (in terms of PPL for example especially after just onen epoch) 
+
+- installed flash att. had to use no cache for hpc
+
+- implementation of the flashattention block 
+
+- run into OOM with Flash Attention for max batch size - will fix tomorrow 
--- lab4/training.sh
+++ lab4/training.sh
@@ -3,11 +3,11 @@
 #SBATCH -A plgar2025-gpu-a100
 #SBATCH -p plgrid-gpu-a100
 #SBATCH -N 1
-#SBATCH --tasks-per-node=4      # max P we'll use below
+#SBATCH --tasks-per-node=4
 #SBATCH --gres=gpu
 #SBATCH -t 00:10:00
-#SBATCH -o training-pipeline.out
-#SBATCH -e training-pipeline.err
+#SBATCH -o training-pipeline_%j.out
+#SBATCH -e training-pipeline_%j.err
 
 
 module purge
@@ -16,6 +16,8 @@ module load CUDA/12.1.1
 
 # activate environment
 source /net/tscratch/people/plgjuliaryb/venvs/comp-lingu/bin/activate
+
+# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 cd ..
 # python train.py
